e03f2403dcf19170b29fb2b65ffd557634609a0c
VinceBiggz <wachirakungu@gmail.com>
2025-06-27 09:27:29 +0300

    Force removed gh-pages embedded git repo from cache

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+// src/main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Vincent Wachira's Portfolio</h1>
+      <p>Cloud | Cybersecurity | Automation | Digital Transformation</p>
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
